ac58d88a870ba0efc61a3cfa2f34e4398bdca16d
maantano <maantano@naver.com>
2023-10-31 23:33:47 +0900

    2023.10.31 브루트포스

---
+++ 2023.10.31 한수(1065)_브루트포스.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+arr = [list(input().rstrip()) for _ in range(n)]
+
+
+def chk(arr):
+	mCnt = 1
+	for i in range(n):
+		cnt = 1
+		for j in range(1,n):
+			if arr[i][j] == arr[i][j-1]:
+				cnt+=1
+			else:
+				cnt = 1
+			mCnt = max(mCnt,cnt)
+		for j in range(1,n):
+			if arr[j][i] == arr[j-1][i]:
+				cnt+=1
+			else:
+				cnt = 1
+			mCnt = max(mCnt,cnt)
+	return mCnt
+
+
+ans = 0
+for i in range(n):
+	for j in range(n):
+		if j + 1 < n:
+			arr[i][j],arr[i][j+1] = arr[i][j+1],arr[i][j]
+			cnt = chk(arr)
+			ans = max(cnt,ans)
+			arr[i][j],arr[i][j+1] = arr[i][j+1],arr[i][j]
+
+		if i + 1 < n:
+			arr[i][j],arr[i+1][j] = arr[i+1][j],arr[i][j]
+			cnt = chk(arr)
+			ans = max(cnt,ans)
+			arr[i][j],arr[i+1][j] = arr[i+1][j],arr[i][j]
+
+print(ans)
