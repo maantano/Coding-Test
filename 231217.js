c6740310244625e6692cdea2d8108a09aac79f46
maantano <maantano@naver.com>
2023-12-18 17:25:07 +0900

    202312118

--- 231217.js
+++ 231217.js
@@ -12,17 +12,13 @@ function solution(keymap, targets) {
       if (!map.has(key[i]) || i + 1 < map.get(key[i])) map.set(key[i], i + 1);
     }
   }
-  console.log(map);
-
   for (const target of targets) {
     let count = 0;
     for (let i = 0; i < target.length; i++) {
       count += map.get(target[i]);
     }
-    console.log(count);
     answer.push(count || -1);
   }
-
   return answer;
 }
 solution(keymap, targets);
