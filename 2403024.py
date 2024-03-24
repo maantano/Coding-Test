885074314cbf44db1ea1e63ec64a707a30f96161
maantano <maantano@naver.com>
2024-03-24 23:51:12 +0900

    24.03.24

--- 2403024.py
+++ 2403024.py
@@ -206,14 +206,101 @@
 # 탈출
 # https://www.acmicpc.net/problem/3055
 
-from collections import deque
+# from collections import deque
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
 
 
-r,c = map(int,input().split())
+# r,c = map(int,input().split())
+
+# visited = [[0] * c for _ in range(r)]
+# g = [list(map(str,input().split())) for _ in range(r)]
+# visited = [[0]*c for _ in range(r)]
+# dx,dy = 0,0
+# sx,sy = 0,0
+# ws,wy = 0,0
+# for i in range(r):
+# 	for j in range(c):
+# 		if g[i][j] == 'D':
+# 			dx,dy = i,j
+# 		if g[i][j] == 'S':
+# 			sx,sy = i,j
+# 		if g[i][j] == 'S':
+# 			sx,sy = i,j
+
+
+
+# q = deque()
+
+# while q:
+# 	x,y = q.popleft()
+# 	visited[x][y] = 1
+
+# 	for i in range(4):
+# 		nx = dx[i] + x
+# 		ny = dy[i] + y
+
+# 		if 0<= nx < r and 0<= ny < c:
+# 			if not visited[nx][ny]:
+# 				if g[nx][ny] == '.':
+# 					q.append((nx,ny))
+# 					visited[nx][ny] = visited[x][y]+1
+
+
+
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+
+# r,c = map(int,input().split())
+# visited = [[-1] * c for _ in range(r)]
+# g = [list(map(str,input())) for _ in range(r)]
+
+# q = deque()
+
+# for x in range(r):
+# 	for y in range(c):
+# 		if g[x][y] == '*':
+# 			q.appendleft((x,y))
+# 		elif g[x][y] == 'S':
+# 			q.append((x,y))
+# 			visited[x][y] = 0
+
+
+# while q:
+# 	x,y = q.popleft()
+# 	cur = g[x][y]
+
+# 	for i in range(4):
+# 		nx = dx[i]+x
+# 		ny = dy[i]+y
+
+# 		if 0<=nx < r and 0<=ny<c:
+# 			if visited[nx][ny] != -1:
+# 				continue
+# 			if g[nx][ny] == '*':
+# 				continue
+# 			if g[nx][ny] == 'X':
+# 				continue
+# 			if cur =='*' and g[nx][ny] == 'D':
+# 				continue
+# 			if cur =='S':
+# 				if g[nx][ny] == 'D':
+# 					print(visited[x][y]+1)
+# 					break
+# 				visited[nx][ny] = visited[x][y] + 1
+# 			g[nx][ny] = cur
+# 			q.append((nx,ny))
+# 	else:
+# 		continue
+# 	break
+# else:
+# 	print('KACTAUS')
 
